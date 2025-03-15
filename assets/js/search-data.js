5d11e619e596149d111fe2a4bdf91d4053d261d6
rfayyazi <52934042+rfayyazi@users.noreply.github.com>
2025-03-15 21:45:23 +0000

    Deploying to gh-pages from @ rfayyazi/rfayyazi.github.io@3506cc7575d8aebb1f2191c90e1e035c05fcfd86 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,38 +430,10 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/al-folio/projects/9_project/";
             },},{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/al-folio/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=Q_nRX9gAAAAJ", "_blank");
         },
       },];
